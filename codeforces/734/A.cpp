2ae7dce5bc0ad2ceea2779ca5a6a18206f6abb89
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2020-12-24 23:07:00 +0200

    Add solution for problem `A - Anton and Danik` Link: https://codeforces.com/contest/734/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/734/submission/102265936

---
+++ codeforces/734/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n; cin >> n;
+	string s; cin >> s;
+	int k_a = 0, k_d = 0;
+	for (int i = 0; i < n; i++)
+		if (s[i] == 'A') k_a++; else k_d++;
+	if (k_a > k_d) cout << "Anton" << endl;
+	if (k_a < k_d) cout << "Danik" << endl;
+	if (k_a == k_d) cout << "Friendship" << endl;
+	return 0;
+}
