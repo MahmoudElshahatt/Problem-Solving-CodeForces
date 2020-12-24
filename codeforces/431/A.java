1cc0b43641a112e0fa3e16d5214776bb60b23bf2
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2020-12-24 23:45:00 +0200

    Add solution for problem `A - Black Square` Link: https://codeforces.com/contest/431/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/431/submission/102267259

--- codeforces/431/A.java
+++ codeforces/431/A.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-
-public class ABlackSquare {
-
-   
-    public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int cal=0;
-int [] n={in.nextInt(),in.nextInt(),in.nextInt(),in.nextInt()};    
-        String s=in.next();
-         for(int i=0;i<s.length();i++)
-         {
-         if(s.charAt(i)=='1') cal+=n[0];
-         if(s.charAt(i)=='2') cal+=n[1];
-         if(s.charAt(i)=='3') cal+=n[2];
-         if(s.charAt(i)=='4') cal+=n[3];
-         }
-         System.out.println(cal);
-    }
-    
+public class BlackSquare
+{
+	public static void main(String[] args)
+	{
+		Scanner z=new Scanner(System.in);
+		int arr[]={z.nextInt(),z.nextInt(),z.nextInt(),z.nextInt()};
+		String s=z.next();
+		int cal=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(Character.getNumericValue(s.charAt(i))==1)
+				cal+=arr[0];
+			else if(Character.getNumericValue(s.charAt(i))==2)
+				cal+=arr[1];
+			else if(Character.getNumericValue(s.charAt(i))==3)
+				cal+=arr[2];
+			else if(Character.getNumericValue(s.charAt(i))==4)
+				cal+=arr[3];
+		}
+		System.out.println(cal);
+	}
 }
