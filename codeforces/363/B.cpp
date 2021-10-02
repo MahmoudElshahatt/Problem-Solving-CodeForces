4f2f31fe167c985f9fab1009a94daacabbbbab08
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2021-10-02 23:03:00 +0200

    Add solution for problem `B - Fence` Link: https://codeforces.com/contest/363/problem/B Tags: brute force, dp, *1100 Ref: https://codeforces.com/contest/363/submission/130599752

--- codeforces/363/B.cpp
+++ codeforces/363/B.cpp
@@ -12,7 +12,7 @@ int main()
     ll n,k,pos;
     cin>>n>>k;
         ll a[n];
-        ll pre[1500002]={0};
+        ll pre[150002]={0};
   for(ll i=1;i<=n;i++){
     cin>>a[i-1];
     pre[i]=pre[i-1]+a[i-1];
@@ -25,5 +25,7 @@ int main()
      }
  }
     cout<<pos-k+1;
+
  return 0;
+
 }
